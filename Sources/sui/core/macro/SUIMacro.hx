058f5d104c62d0e763557ad0b5c0d0c5f4c439e8
smoothie-ws <kibot.email@gmail.com>
2024-11-27 11:12:16 +1000

    Extend macro with `alias` and `readonly` keywords

--- Sources/sui/core/macro/SUIMacro.hx
+++ Sources/sui/core/macro/SUIMacro.hx
@@ -12,101 +12,155 @@ class SUIMacro {
 		var fields:Array<Field> = Context.getBuildFields();
 		for (field in fields)
 			if (field.meta != null)
-				for (meta in field.meta) {
-					if (meta.name == "readonly")
-						switch (field.kind) {
-							case FVar(varType, e):
-								field.meta.push({name: ":isVar", pos: Context.currentPos()});
-								field.kind = FProp("default", "never", varType, e);
-							case _:
-								null;
-						}
-					if (meta.name == "observable")
-						switch (field.kind) {
-							case FVar(varType, e):
-								field.meta.push({name: ":isVar", pos: Context.currentPos()});
-								field.kind = FProp("default", "set", varType, e);
-
-								var listenersName = '${field.name}Listeners';
-								fields.push({
-									pos: Context.currentPos(),
-									name: listenersName,
-									access: [APrivate],
-									kind: FVar(macro :Array<$varType->Void>, macro [])
-								});
-
-								var listenersRef = listenersName.resolve();
-								fields.push({
-									pos: Context.currentPos(),
-									name: 'add${field.name.capitalize()}Listener',
-									kind: FFun({
-										ret: macro :Int,
-										args: [
-											{
-												name: 'f',
-												type: macro :$varType->Void,
-												opt: false
-											}
-										],
-										expr: macro {
-											$listenersRef.push($i{'f'});
-											return $listenersRef.length - 1;
-										}
-									}),
-									access: [ADynamic, APublic]
-								});
-
-								fields.push({
-									pos: Context.currentPos(),
-									name: 'remove${field.name.capitalize()}Listener',
-									kind: FFun({
-										args: [
-											{
-												name: 'listenerID',
-												type: macro :Int,
-												opt: false
-											}
-										],
-										expr: macro {
-											$listenersRef.splice(listenerID, 1);
-										}
-									}),
-									access: [ADynamic, APublic]
-								});
-
-								var variableRef = field.name.resolve();
-								var setterBody = macro {
-									var from = $variableRef;
-									$variableRef = v;
-									for (f in $listenersRef)
-										f(v);
-									return $variableRef;
-								};
-								fields.push({
-									pos: Context.currentPos(),
-									name: 'set_${field.name}',
-									access: [APrivate],
-									meta: [],
-									kind: FFun({
-										ret: varType,
-										params: [],
-										expr: setterBody,
-										args: [
-											{
-												value: null,
-												type: varType,
-												opt: false,
-												name: "v"
-											}
-										]
-									}),
-									doc: ""
-								});
-							case _:
-								null;
-						}
-				}
+				for (meta in field.meta)
+					switch (meta.name) {
+						case "alias":
+							buildAlias(field, fields);
+						case "readonly":
+							buildReadonly(field, fields);
+						case "observable":
+							buildObservable(field, fields);
+						case _:
+							null;
+					}
 
 		return fields;
+	}
+
+	public static inline function buildAlias(field:Field, fields:Array<Field>) {
+		switch (field.kind) {
+			case FVar(varType, e):
+				field.meta.push({name: ":isVar", pos: Context.currentPos()});
+				field.kind = FProp("get", "set", varType, e);
+				fields.push({
+					pos: Context.currentPos(),
+					name: 'get_${field.name}',
+					access: [AInline, APrivate],
+					meta: [],
+					kind: FFun({
+						ret: null,
+						expr: macro {
+							return $e;
+						},
+						args: []
+					})
+				});
+				fields.push({
+					pos: Context.currentPos(),
+					name: 'set_${field.name}',
+					access: [AInline, APrivate],
+					meta: [],
+					kind: FFun({
+						ret: varType,
+						expr: macro {
+							$e = v;
+							return v;
+						},
+						args: [
+							{
+								value: null,
+								type: varType,
+								opt: false,
+								name: "v"
+							}
+						]
+					})
+				});
+			case _:
+				null;
+		}
+	}
+
+	public static inline function buildReadonly(field:Field, fields:Array<Field>) {
+		switch (field.kind) {
+			case FVar(varType, e):
+				field.meta.push({name: ":isVar", pos: Context.currentPos()});
+				field.kind = FProp("default", "never", varType, e);
+			case _:
+				null;
+		}
+	}
+
+	public static inline function buildObservable(field:Field, fields:Array<Field>) {
+		switch (field.kind) {
+			case FVar(varType, e):
+				field.meta.push({name: ":isVar", pos: Context.currentPos()});
+				field.kind = FProp("default", "set", varType, e);
+
+				var listenersName = '${field.name}Listeners';
+				fields.push({
+					pos: Context.currentPos(),
+					name: listenersName,
+					access: [APrivate],
+					kind: FVar(macro :Array<$varType->Void>, macro [])
+				});
+
+				var listenersRef = listenersName.resolve();
+				fields.push({
+					pos: Context.currentPos(),
+					name: 'add${field.name.capitalize()}Listener',
+					kind: FFun({
+						ret: macro :Int,
+						args: [
+							{
+								name: 'f',
+								type: macro :$varType->Void,
+								opt: false
+							}
+						],
+						expr: macro {
+							$listenersRef.push($i{'f'});
+							return $listenersRef.length - 1;
+						}
+					}),
+					access: [ADynamic, APublic]
+				});
+
+				fields.push({
+					pos: Context.currentPos(),
+					name: 'remove${field.name.capitalize()}Listener',
+					kind: FFun({
+						args: [
+							{
+								name: 'listenerID',
+								type: macro :Int,
+								opt: false
+							}
+						],
+						expr: macro {
+							$listenersRef.splice(listenerID, 1);
+						}
+					}),
+					access: [ADynamic, APublic]
+				});
+
+				var variableRef = field.name.resolve();
+				fields.push({
+					pos: Context.currentPos(),
+					name: 'set_${field.name}',
+					access: [AInline, APrivate],
+					meta: [],
+					kind: FFun({
+						ret: varType,
+						expr: macro {
+							var from = $variableRef;
+							$variableRef = v;
+							for (f in $listenersRef)
+								f(v);
+							return $variableRef;
+						},
+						args: [
+							{
+								value: null,
+								type: varType,
+								opt: false,
+								name: "v"
+							}
+						]
+					}),
+				});
+			case _:
+				null;
+		}
 	}
 }
