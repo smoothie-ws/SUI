fceef436bef0d24336cad84174d0639752edf4ca
smoothie-ws <kibot.email@gmail.com>
2024-11-28 14:50:49 +1000

    Extend `ColumnLayout` and `RowLayout`

---
+++ Sources/sui/elements/layouts/ColumnLayout.hx
@@ -0,0 +1,52 @@
+package sui.elements.layouts;
+
+import kha.FastFloat;
+// sui
+import sui.elements.Element;
+import sui.positioning.Alignment;
+import sui.positioning.Direction;
+
+using sui.core.utils.ArrayExt;
+
+class ColumnLayout extends Element {
+	@:isVar public var spacing(default, set):FastFloat = 0.;
+	@:isVar public var direction(default, set):Direction = Direction.TopToBottom;
+	public var alignment:Alignment = Alignment.HCenter | Alignment.VCenter;
+
+	inline function set_spacing(value:FastFloat) {
+		spacing = value;
+		for (i in 1...children.length)
+			children[i].anchors.topMargin = spacing;
+		return value;
+	}
+
+	inline function set_direction(value:Direction) {
+		if (direction == Direction.BottomToTop && value == Direction.TopToBottom)
+			children.reverse();
+		buildLayout();
+		direction = value;
+		return value;
+	}
+
+	override inline function construct() {
+		if (direction == Direction.BottomToTop)
+			children.reverse();
+		buildLayout();
+	}
+
+	inline function buildLayout() {
+		var h = height / children.length;
+		children[0].anchors.top = top;
+		children[0].height = h;
+		if ((alignment & Alignment.VCenter) != 0)
+			children[0].anchors.verticalCenter = verticalCenter;
+
+		for (i in 1...children.length) {
+			children[i].anchors.top = children[i - 1].top;
+			children[i].anchors.topMargin = spacing;
+			children[i].height = h;
+			if ((alignment & Alignment.VCenter) != 0)
+				children[i].anchors.verticalCenter = verticalCenter;
+		}
+	}
+}
--- Sources/sui/elements/layouts/RowLayout.hx
+++ Sources/sui/elements/layouts/RowLayout.hx
@@ -9,27 +9,44 @@ import sui.positioning.Direction;
 using sui.core.utils.ArrayExt;
 
 class RowLayout extends Element {
-	public var spacing:FastFloat = 0.;
-	public var direction:Direction = Direction.TopToBottom;
+	@:isVar public var spacing(default, set):FastFloat = 0.;
+	@:isVar public var direction(default, set):Direction = Direction.LeftToRight;
 	public var alignment:Alignment = Alignment.HCenter | Alignment.VCenter;
 
-	override inline function construct() {
-		if (direction == Direction.BottomToTop)
+	inline function set_spacing(value:FastFloat) {
+		spacing = value;
+		for (i in 1...children.length)
+			children[i].anchors.leftMargin = spacing;
+		return value;
+	}
+
+	inline function set_direction(value:Direction) {
+		if (direction == Direction.LeftToRight && value == Direction.RightToLeft || direction == Direction.RightToLeft && value == Direction.LeftToRight)
 			children.reverse();
+		buildLayout();
+		direction = value;
+		return value;
+	}
 
-		var h = height / children.length;
+	override inline function construct() {
+		if (direction == Direction.RightToLeft)
+			children.reverse();
+		buildLayout();
+	}
 
-		children[0].anchors.top = top;
-		children[0].height = h;
-		if ((alignment & Alignment.VCenter) != 0)
-			children[0].anchors.verticalCenter = verticalCenter;
+	inline function buildLayout() {
+		var w = width / children.length;
+		children[0].anchors.left = left;
+		children[0].width = w;
+		if ((alignment & Alignment.HCenter) != 0)
+			children[0].anchors.horizontalCenter = horizontalCenter;
 
 		for (i in 1...children.length) {
-			children[i].anchors.top = children[i - 1].top;
-			children[i].anchors.topMargin = spacing;
-			children[i].height = h;
-			if ((alignment & Alignment.VCenter) != 0)
-				children[i].anchors.verticalCenter = verticalCenter;
+			children[i].anchors.left = children[i - 1].left;
+			children[i].anchors.leftMargin = spacing;
+			children[i].width = w;
+			if ((alignment & Alignment.HCenter) != 0)
+				children[i].anchors.horizontalCenter = horizontalCenter;
 		}
 	}
 }
